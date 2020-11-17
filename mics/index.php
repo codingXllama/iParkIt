c2266db33d05f70054dd7d59f0e59c8b1c4d8bd5
Osama Al-Chalabi <alchala@uwindsor.ca>
2020-11-16 23:37:36 -0500

    dirc changes

--- mics/index.php
+++ mics/index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
-    <link rel="stylesheet" href="/assets/css/style.min.css">
+    <link rel="stylesheet" href="./assets/css/style.min.css">
 
 
 </head>
